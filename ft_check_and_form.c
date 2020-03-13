e45ce45feb7b2c51e0e19e38c772e98421e3dd17
Eino Palomaki <eino.palomaki@icloud.com>
2020-03-13 12:35:58 +0200

    done fillit project, needs some adjustments and optimisatiion

---
+++ ft_check_and_form.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_and_form.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epalomak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/11 15:53:13 by epalomak          #+#    #+#             */
+/*   Updated: 2019/12/18 13:09:17 by epalomak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+int		ft_character_check(char *str)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == '#')
+			count++;
+		if (str[i] != '.' && str[i] != '#')
+			return (-1);
+		i++;
+	}
+	if (count != 4)
+		return (-1);
+	return (0);
+}
+
+int		ft_check_tetri(char *str)
+{
+	int		i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	if (ft_character_check(str) == -1)
+		return (-1);
+	while (str[i])
+	{
+		if (str[i] == '#' && str[i + 1] == '#' && (i != 3 && i != 7 && i != 11)
+				&& i + 1 < 16)
+			count++;
+		if (str[i] == '#' && str[i - 1] == '#' && (i != 4 && i != 8 && i != 12)
+				&& i - 1 >= 0)
+			count++;
+		if (str[i] == '#' && str[i + 4] == '#' && i < 12 && i + 4 < 16)
+			count++;
+		if (str[i] == '#' && str[i - 4] == '#' && i - 4 >= 0)
+			count++;
+		i++;
+	}
+	if (count != 6 && count != 8)
+		return (-1);
+	return (1);
+}
+
+int		ft_form_tetrostr(int fd, char *ptr, char **str)
+{
+	int		i;
+	char	*str2;
+
+	i = -1;
+	while (++i <= 4 && get_next_line(fd, &str2) > 0)
+	{
+		if ((i < 4 && ft_strlen(str2) != 4) || (i == 4 && ft_strlen(str2) != 0))
+		{
+			free(str2);
+			return (-1);
+		}
+		else if (i == 4)
+		{
+			free(str2);
+			return (1);
+		}
+		ptr = *str;
+		*str = ft_strjoin(*str, str2);
+		free(ptr);
+		free(str2);
+	}
+	if (i == 3 || ft_strlen(str2) != 0)
+		return (ft_free_line(str2));
+	free(str2);
+	return (0);
+}
+
+int		ft_free_line(char *line)
+{
+	free(line);
+	return (-1);
+}
+
+int		ft_check_n_form(int fd, t_list2 **head)
+{
+	int		rt;
+	char	*ptr;
+	char	*line;
+	int		count;
+
+	rt = 1;
+	ptr = NULL;
+	count = 0;
+	while (rt > 0)
+	{
+		line = ft_strnew(0);
+		if ((rt = ft_form_tetrostr(fd, ptr, &line)) == -1)
+			return (ft_free_line(line));
+		if (rt == 0 && line[0] == '\0')
+			return (-1);
+		if (ft_check_tetri(line) == -1 || count > 25)
+			return (ft_free_line(line));
+		ft_list(head, line, count++);
+		free(line);
+	}
+	return (ft_sqr_root(count));
+}
